8f036c5d71825a07552ddd5a0a5814cab3af1f98
Jim Hough <teachlife@outlook.com>
2019-04-15 19:28:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,12 @@ class Dog
     @dog_name = name
   end
   def name
-    puts @dog_name
+    @dog_name
   end
   def breed= (breed)
     @breed_name = breed
   end
   def breed
-    puts @breed_name
+    @breed_name
   end
 end
