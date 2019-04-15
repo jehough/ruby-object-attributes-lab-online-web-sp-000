e54ef4b94592b0650afa114f8deb0be209e3f04e
Jim Hough <teachlife@outlook.com>
2019-04-15 19:27:30 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name = (name)
+    @dog_name = name
+  end
+  def name
+    puts @dog_name
+  end
+  def breed = (breed)
+    @breed_name = breed
+  end
+  def breed
+    puts @breed_name
+  end
+end
